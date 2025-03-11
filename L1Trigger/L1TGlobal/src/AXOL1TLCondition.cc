02894485fcfa11687448800d10e80de9d91dc14c
Melissa Quinnan <mkq5008@gmail.com>
2025-03-11 06:04:58 +0100

    v5 changes

--- L1Trigger/L1TGlobal/src/AXOL1TLCondition.cc
+++ L1Trigger/L1TGlobal/src/AXOL1TLCondition.cc
@@ -41,16 +41,6 @@
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/MessageLogger/interface/MessageDrop.h"
 
-namespace {
-  //template function for reading results
-  template <typename ResultType, typename LossType>
-  LossType readResult(hls4mlEmulator::Model& model) {
-    std::pair<ResultType, LossType> ADModelResult;  //model outputs a pair of the (result vector, loss)
-    model.read_result(&ADModelResult);
-    return ADModelResult.second;
-  }
-}  // namespace
-
 l1t::AXOL1TLCondition::AXOL1TLCondition()
     : ConditionEvaluation(), m_gtAXOL1TLTemplate{nullptr}, m_gtGTB{nullptr}, m_model{nullptr} {}
 
@@ -140,7 +130,13 @@ const bool l1t::AXOL1TLCondition::evaluateCondition(const int bxEval) const {
 
   //types of inputs and outputs
   typedef ap_fixed<18, 13> inputtype;
+  typedef ap_fixed<18, 14, AP_RND_CONV, AP_SAT> resulttype;  //v5 default
+  if ((m_model_loader.model_name() == "GTADModel_v3") || (m_model_loader.model_name() == "GTADModel_v4")) {
+    typedef std::array<ap_fixed<10, 7, AP_RND_CONV, AP_SAT>, 8> resulttype;  //v3/v4 overwrite
+  }
   typedef ap_ufixed<18, 14> losstype;
+  typedef std::pair<resulttype, losstype> pairtype;
+  // typedef std::array<ap_fixed<10, 7>, 13> resulttype;  //deprecated v1 type:
 
   //define zero
   inputtype fillzero = 0.0;
@@ -155,10 +151,10 @@ const bool l1t::AXOL1TLCondition::evaluateCondition(const int bxEval) const {
   inputtype EtSumInput[EtSumVecSize];
 
   //declare result vectors +score
-  // resulttype result;
+  resulttype result;
   losstype loss;
-  // pairtype ADModelResult;  //model outputs a pair of the (result vector, loss)
-  float score = -1.0;  //not sure what the best default is hm??
+  pairtype ADModelResult;  //model outputs a pair of the (result vector, loss)
+  float score = -1.0;      //not sure what the best default is hm??
 
   //check number of input objects we actually have (muons, jets etc)
   int NCandMu = candMuVec->size(useBx);
@@ -241,18 +237,10 @@ const bool l1t::AXOL1TLCondition::evaluateCondition(const int bxEval) const {
   //now run the inference
   m_model->prepare_input(ADModelInput);  //scaling internal here
   m_model->predict();
-  // m_model->read_result(&ADModelResult);  // this should be the square sum model result
-  if ((m_model_loader.model_name() == "GTADModel_v3") ||
-      (m_model_loader.model_name() == "GTADModel_v4")) {  //v3/v4 overwrite
-    using resulttype = std::array<ap_fixed<10, 7, AP_RND_CONV, AP_SAT>, 8>;
-    loss = readResult<resulttype, losstype>(*m_model);
-  } else {  //v5 default
-    using resulttype = ap_fixed<18, 14, AP_RND_CONV, AP_SAT>;
-    loss = readResult<resulttype, losstype>(*m_model);
-  }
+  m_model->read_result(&ADModelResult);  // this should be the square sum model result
 
-  // result = ADModelResult.first;
-  // loss = ADModelResult.second;
+  result = ADModelResult.first;
+  loss = ADModelResult.second;
   score = ((loss).to_float()) * 16.0;  //scaling to match threshold
   //save score to class variable in case score saving needed
   setScore(score);
