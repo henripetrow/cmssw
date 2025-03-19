6289b4c1b4e11189ce5f4fcde3de05d1aa6d6315
Antonio Linares <a.linares@cern.ch>
2025-03-19 10:27:39 +0100

    Backport Raw Skims to CMSSW_15_0_X

--- Configuration/DataProcessing/python/Repack.py
+++ Configuration/DataProcessing/python/Repack.py
@@ -79,22 +79,16 @@ def repackProcess(**args):
 
         if rawSkim:
 
-            skim = getattr(RawSkims, rawSkim)
-            setattr(process, rawSkim, skim)
-
             selectEventsBase = [item.replace(":HLT", "") for item in selectEventsBase]
 
-            baseSelection = hlt.hltHighLevel.clone(
+            process.baseSelection = hlt.hltHighLevel.clone(
                 TriggerResultsTag = "TriggerResults::HLT",
                 HLTPaths = cms.vstring(selectEventsBase)
             )
-
-            path = cms.Path(skim + baseSelection)
-            
-            baseSelectionName = output['moduleLabel'].split('_')[1] + f'_{rawSkim}'
-
-            setattr(process, baseSelectionName, baseSelection)
-            selectEvents = f"{baseSelectionName}Path"
+            skim = getattr(RawSkims, rawSkim)
+            setattr(process, rawSkim, skim)
+            path = cms.Path(skim + process.baseSelection)
+            selectEvents = f"{rawSkim}Path"
             setattr(process, selectEvents, path)
 
         else:
